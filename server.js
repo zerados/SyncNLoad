0b1239e3c0b0a0c457f2fe770ed73ccec18b61a7
Khadeus <erickofiyork@gmail.com>
2015-12-03 12:05:16 +0100

    minor variable name change

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ function download(url, callback) {
 
 function validDuration(duration) {
 	var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
-        var hours = 0, minutes = 0, seconds = 0, totalseconds;
+        var hours = 0, minutes = 0, seconds = 0, totalInSeconds;
         var maxDurationInSeconds = 1800;
 
         if (reptms.test(duration)) {
@@ -174,8 +174,8 @@ function validDuration(duration) {
             if (matches[1]) hours = Number(matches[1]);
             if (matches[2]) minutes = Number(matches[2]);
             if (matches[3]) seconds = Number(matches[3]);
-            totalseconds = hours * 3600  + minutes * 60 + seconds;
-        	if (totalseconds < maxDurationInSeconds) {
+            totalInSeconds = hours * 3600  + minutes * 60 + seconds;
+        	if (totalInSeconds < maxDurationInSeconds) {
         		return true;
            	}
       	}
