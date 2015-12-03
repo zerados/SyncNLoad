27d5175b44e1942acffe75e72312a32eac1239b4
zerados <mort065b@stud.kea.dk>
2015-12-03 13:26:58 +0100

    latest commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var videoPlaylist = [];
 var database = config.dbUrl;
 
 //functions
-server.all('/', function(req, res, next) {
+server.all('*', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
