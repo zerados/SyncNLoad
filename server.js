bd08d5b8fc9b87b477e0f6fd7db195170aee28bf
zerados <mort065b@stud.kea.dk>
2015-12-10 12:28:15 +0100

    merged stuff

--- server.js
+++ server.js
@@ -25,6 +25,16 @@ app.all('*', function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
 });
+app.get('/video/archive', function (req, res) {
+	mongodb.connect(database, function (err, db) {
+		var collection = db.collection('addedVideos');
+		collection.find({$or: [{'status' : 'played'}, {'status' : 'deleted'}]}).toArray(function (err, result) {
+			videoPlaylist = result;
+			db.close();
+			res.json(result);
+		});
+	});
+})
 app.get('/video/:index', function (req, res, next) {
 	res.json(videoPlaylist[req.params.index]);
 	next();
@@ -39,13 +49,47 @@ app.delete('/video/:index', function (req, res) {
 	var videoToBeRemoved = videoPlaylist[req.params.index];
 
 	videoPlaylist.splice(videoToBeRemoved, 1);
+	status = {"status" : "deleted"};
+	console.log("DOING STUPID SHIT : " + req.params.index)
+	if(req.params.index === "0"){
+		console.log("DOING STUPID SHIT : " + req.params.index)
+		status.status = "played";
+	}
+	mongodb.connect(database, function (err, db) {
+		var collection = db.collection('addedVideos');
 
+		collection.updateOne({"_id" : videoToBeRemoved._id}, {$set : status}, function (err, result) {
+			console.log("Deleted the following object in the database: " + JSON.stringify(result));
+			db.close();
+		});
+	});
 	if (videoToBeRemoved !== videoPlaylist[req.params.id]) {
 		//statuscode for successful deletion...
 		res.status(200).send();
 	} else {
 		//statuscode for failure...
 		res.status(404).send();
+	}
+})
+app.delete('/videos', function (req, res) {
+
+	mongodb.connect(database, function (err, db) {
+		var collection = db.collection('addedVideos');
+
+		collection.updateMany({"status" : "unPlayed"}, {$set: {"status" : "deleted"}}, {multi : true}, function (err, result) {
+			console.log("deleted: " + JSON.stringify(result));
+			db.close();
+		});
+	});
+
+	videoPlaylist = [];
+
+	if (videoPlaylist.length === 0) {
+		//statuscode for successful deletion...
+		res.status(200).send();
+	} else {
+		//statuscode for failure...
+		res.status(500).send();
 	}
 })
 app.post('/video/:id', function (req, res, next) {
@@ -67,16 +111,17 @@ app.post('/video/:id', function (req, res, next) {
 				"player" : jsonData.items[0].player.embedHtml,
 				"duration" : jsonData.items[0].contentDetails.duration,
 				"url" : "https://www.youtube.com/watch?v=" + jsonData.items[0].id,
-				"played" : "unPlayed"
+				"status" : "unPlayed"
 			}
 			//add videoObject to the playlist array.
-			videoPlaylist.push(videoObject);
+
 
 			mongodb.connect(database, function (err, db) {
 				var collection = db.collection('addedVideos');
 
 				collection.insert(videoObject, function (err, result) {
-					console.log("Logged the following object in the database: " + JSON.stringify(result));
+					console.log("Logged the following object in the database: " + JSON.stringify(result.ops[0]));
+					videoPlaylist.push(result.ops[0]);
 					db.close();
 				});
 			});
@@ -192,7 +237,7 @@ function init(){
 	mongodb.connect(database, function (err, db) {
 		var collection = db.collection('addedVideos');
 
-		collection.find({"played" : "unPlayed"}).toArray(function (err, result) {
+		collection.find({"status" : "unPlayed"}).toArray(function (err, result) {
 			videoPlaylist = result;
 			db.close();
 		});
